9a993c6fa0581d002854b3f88dee833e873c85c1
xavierjuli <julianaxavier477@live.com>
2025-07-10 23:08:59 -0300

    Create MarcarConcluidaController.java

---
+++ GUI/MarcarConcluidaController.java
@@ -0,0 +1,28 @@
+package GUI;
+
+import model.Tarefa;
+import service.TarefaService;
+
+public class MarcarConcluidaController {
+
+    private final TarefaService tarefaService;
+
+    public MarcarConcluidaController(TarefaService tarefaService) {
+        this.tarefaService = tarefaService;
+    }
+
+    public boolean marcarConcluida(Tarefa tarefa) {
+        if (tarefa == null || tarefa.isConcluida()) {
+            System.out.println("Não foi possível fazer essa operação");
+            return false;
+        }
+
+        boolean sucesso = tarefaService.atualizarStatus(tarefa.getId(), true);
+
+        if (sucesso) {
+            tarefa.setConcluida(true);
+        }
+
+        return sucesso;
+    }
+}
